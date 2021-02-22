ba9a010d73238ea3a3f7d9bce060d87b2dc05804
Martin Pätzold <mac@mpaetzold.de>
2021-02-22 20:38:49 +0100

    Try & Error...the 3rd 🙈

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 const spicedPG = require("spiced-pg");
 //const db = spicedPG("postgres:martinpaetzold:@localhost:5432/petition");
-const dbSpicedPG = spicedPG("postgres:martinpaetzold:@localhost:5432/petition");
 //heroku || local
-const db = process.env.DATABASE_URL || dbSpicedPG;
+const db =
+    process.env.DATABASE_URL ||
+    "postgres:martinpaetzold:@localhost:5432/petition";
 
 //supposed to add a signature to the database
 exports.addSignature = (userId, signatureCode) => {
