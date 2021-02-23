e7731261be5234161ebbc25b158d5255c3c54df1
Martin Pätzold <mac@mpaetzold.de>
2021-02-23 14:24:34 +0100

    Bugfix Heroku..

--- database.js
+++ database.js
@@ -1,11 +1,9 @@
 const spicedPG = require("spiced-pg");
-const db = spicedPG("postgres:martinpaetzold:@localhost:5432/petition");
+//const db = spicedPG("postgres:martinpaetzold:@localhost:5432/petition");
 //heroku || local
-// doesn't work yet
-/* const db =
-    process.env.DATABASE_URL ||
-    "postgres:martinpaetzold:@localhost:5432/petition";
-*/
+const db = spicedPG(
+    process.env.DATABASE_URL || "postgres:florian:@localhost:5432/petition"
+);
 
 //supposed to add a signature to the database
 exports.addSignature = (userId, signatureCode) => {
